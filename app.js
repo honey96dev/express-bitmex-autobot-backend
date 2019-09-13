894fc6163aa022f769b7e398e9e506c726178741
honey96dev <honey96dev@gmail.com>
2019-09-12 22:50:20 -0800

    Update app.js

--- app.js
+++ app.js
@@ -33,37 +33,38 @@ app.use(compression());
 // app.use('/assets', express.static(path.join(__dirname, 'public')));
 
 app.use('/api', expressJwt({secret: session.secret})
-    .unless({
-        path: [
-            '/api/auth/sign-in',
-            '/api/auth/sign-up',
-            /\/api\/market-sentiment\/one\/*/,
-            /\/api\/market-sentiment\/collection\/*/,
-            /\/api\/deribit\/data\/*/,
-            /\/api\/verify\/*/,
-        ]
-    }));
+  .unless({
+    path: [
+      '/api/auth/sign-in',
+      '/api/auth/sign-up',
+      /\/api\/market-sentiment\/one\/*/,
+      /\/api\/market-sentiment\/collection\/*/,
+      /\/api\/deribit\/data\/*/,
+      /\/api\/verify\/*/,
+      /\/api\/signal-bot\/order\/*/,
+    ]
+  }));
 
 app.use('/api', apiRouter);
 app.use(express.static(path.join(__dirname, 'frontend')));
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'frontend/index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/index.html'));
 });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 export default app;
